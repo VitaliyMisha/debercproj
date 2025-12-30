05c02eed5ad43e8982af1d5a021febab2a7a62d3
Vitalii Misha <vitaliymisha@gmail.com>
2025-12-30 18:38:53 +0200

    few changes )

--- vite.config.ts
+++ vite.config.ts
@@ -7,27 +7,36 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
             manifest: {
                 name: 'Deberc Score App',
-                short_name: 'Деберць',
-                description: 'Додаток для ведення рахунку в Деберці',
+                short_name: 'Deberc',
+                start_url: '/',
+                display: 'standalone',
+                background_color: '#ffffff',
                 theme_color: '#4f46e5',
                 icons: [
                     {
-                        src: 'favicon-32x32.png',
-                        sizes: '32x32',
+                        src: 'icons/icon-192x192.png',
+                        sizes: '192x192',
                         type: 'image/png'
                     },
                     {
-                        src: 'favicon-32x32.png',
-                        sizes: '192x192',
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
                         type: 'image/png'
+                    },
+                    {
+                        src: 'icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
                     }
                 ]
             }
         })
-    ],
-    server: {
-        port: 5173,
-    },
+    ]
 });
